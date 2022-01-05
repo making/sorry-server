0436c5215ce8f87079ea034e5050cbdddb53d8af
Toshiaki Maki <tmaki@vmware.com>
2022-01-05 13:57:00 +0900

    Use 503 instead of 200

--- src/main/java/am/ik/sorry/SorryHandler.java
+++ src/main/java/am/ik/sorry/SorryHandler.java
@@ -1,31 +1,33 @@
 package am.ik.sorry;
 
+import java.util.Locale;
+
+import reactor.core.publisher.Mono;
+
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Mono;
 
-import java.util.Locale;
-
+import static org.springframework.http.HttpStatus.SERVICE_UNAVAILABLE;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 public class SorryHandler {
 
-    public RouterFunction<ServerResponse> routes() {
-        return route() //
-            .route(x -> true, this::sorry)
-            .build();
-    }
+	public RouterFunction<ServerResponse> routes() {
+		return route() //
+				.route(x -> true, this::sorry)
+				.build();
+	}
 
-    Mono<ServerResponse> sorry(ServerRequest req) {
-        final Locale locale = req.exchange().getLocaleContext().getLocale();
-        final String message;
-        if (locale != null && "ja".equalsIgnoreCase(locale.getLanguage())) {
-            message = "現在メンテナンス中です。ご迷惑をおかけします。";
-        } else {
-            message = "Server under maintenance, sorry for the inconveniences.";
-        }
-        return ok().bodyValue(message);
-    }
+	Mono<ServerResponse> sorry(ServerRequest req) {
+		final Locale locale = req.exchange().getLocaleContext().getLocale();
+		final String message;
+		if (locale != null && "ja".equalsIgnoreCase(locale.getLanguage())) {
+			message = "現在メンテナンス中です。ご迷惑をおかけします。";
+		}
+		else {
+			message = "Server under maintenance, sorry for the inconveniences.";
+		}
+		return ServerResponse.status(SERVICE_UNAVAILABLE).bodyValue(message);
+	}
 }
